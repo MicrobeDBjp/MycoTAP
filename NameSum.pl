d43c410ff2b5580fb88f3d18650b74dd681e3e17
Hiroshi Mori <hmori@nig.ac.jp>
2021-04-02 08:39:50 +0900

    Add files via upload

---
+++ NameSum.pl
@@ -0,0 +1,39 @@
+#! /usr/local/bin/perl -w
+use warnings;
+use strict;
+
+my ($input, $output, $aa) = 0;
+my (%id_Hash) = ();
+
+$input = $ARGV[0];
+open(INPUT, "$input") or die "Can't open \"$input\"\n";
+
+$output = "$input.species.txt";
+open(OUTPUT, ">$output") or die "Can't open \"$output\"\n";
+
+open(INPUT, "$input") or die "Can't open \"$input\"\n";
+while(<INPUT>){
+    if(/^\S+\s+(\S+?\_\S+?)\|\S+?\s+/){
+        if(exists($id_Hash{$1})){
+            $id_Hash{$1} += 1;
+        }
+        else{
+            $id_Hash{$1} = 1;
+        }
+    }
+    elsif(/^\S+\s+(\S+?\_\S+?)\_\S+?\s+/){
+        if(exists($id_Hash{$1})){
+            $id_Hash{$1} += 1;
+        }
+        else{
+            $id_Hash{$1} = 1;
+        }
+    }
+}
+close INPUT;
+
+foreach $aa (sort keys %id_Hash){
+    print OUTPUT "$aa\t$id_Hash{$aa}\n";
+}
+close OUTPUT;
+exit;
