1f7e3e0c2554fccd8daaa9b94c1c9a4ce83708e3
Hiroshi Mori <hmori@nig.ac.jp>
2021-04-02 11:14:05 +0900

    Update MycoTAPLocal.sh

--- ForLocalComputer/MycoTAPLocal.sh
+++ ForLocalComputer/MycoTAPLocal.sh
@@ -2,7 +2,7 @@ fastp -i "$1" -o  "$1".rem.fastq -G -3 -n 1 -l 90 -w 1 -x
 seqkit fq2fa "$1".rem.fastq > "$1".rem.fastq.fasta
 vsearch-2.15.0-linux-x86_64/bin/vsearch --usearch_global "$1".rem.fastq.fasta --db UNITERefS.20200204.fasta.withspecies.fasta --id 0.97 --query_cov 0.8 --strand both --blast6out "$1".rem.fastq.fasta.UNITE.97withspecies
 perl NameSum.pl "$1".rem.fastq.fasta.UNITE.97withspecies
-/work1/src/vsearch-2.15.0-linux-x86_64/bin/vsearch --usearch_global "$1".rem.fastq.fasta --db UNITERefS.20200204.fasta.sp.fasta --id 0.97 --query_cov 0.8 --strand both --blast6out "$1".rem.fastq.fasta.UNITE.97nospecies
+vsearch-2.15.0-linux-x86_64/bin/vsearch --usearch_global "$1".rem.fastq.fasta --db UNITERefS.20200204.fasta.sp.fasta --id 0.97 --query_cov 0.8 --strand both --blast6out "$1".rem.fastq.fasta.UNITE.97nospecies
 perl NameSum.pl "$1".rem.fastq.fasta.UNITE.97nospecies
 perl RemoveNameDup.pl "$1".rem.fastq.fasta.UNITE.97nospecies "$1".rem.fastq.fasta.UNITE.97withspecies
 perl NameSum.pl "$1".rem.fastq.fasta.UNITE.97nospecies.withoutdup
