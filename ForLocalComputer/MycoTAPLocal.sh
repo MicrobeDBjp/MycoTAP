3d5aeb8dc6e8c23dd559adafc982ba3772846c20
Hiroshi Mori <hmori@nig.ac.jp>
2021-04-02 11:50:41 +0900

    Update MycoTAPLocal.sh

--- ForLocalComputer/MycoTAPLocal.sh
+++ ForLocalComputer/MycoTAPLocal.sh
@@ -1,8 +1,43 @@
-fastp -i "$1" -o  "$1".rem.fastq -G -3 -n 1 -l 90 -w 1 -x
-seqkit fq2fa "$1".rem.fastq > "$1".rem.fastq.fasta
-vsearch-2.15.0-linux-x86_64/bin/vsearch --usearch_global "$1".rem.fastq.fasta --db UNITERefS.20200204.fasta.withspecies.fasta --id 0.97 --query_cov 0.8 --strand both --blast6out "$1".rem.fastq.fasta.UNITE.97withspecies
-perl NameSum.pl "$1".rem.fastq.fasta.UNITE.97withspecies
-vsearch-2.15.0-linux-x86_64/bin/vsearch --usearch_global "$1".rem.fastq.fasta --db UNITERefS.20200204.fasta.sp.fasta --id 0.97 --query_cov 0.8 --strand both --blast6out "$1".rem.fastq.fasta.UNITE.97nospecies
-perl NameSum.pl "$1".rem.fastq.fasta.UNITE.97nospecies
-perl RemoveNameDup.pl "$1".rem.fastq.fasta.UNITE.97nospecies "$1".rem.fastq.fasta.UNITE.97withspecies
-perl NameSum.pl "$1".rem.fastq.fasta.UNITE.97nospecies.withoutdup
+#!/bin/sh
+# USAGE: this_shell_script input_file output_dir
+# input_file=$1
+# output_dir=$2
+UNITEREF1=/home/hoge/UNITERefS.20200204.fasta.withspecies.fasta.NoN.fasta
+UNITEREF2=/home/hoge/UNITERefS.20200204.fasta.sp.fasta.NoN.fasta
+PROGRAMDIR=/home/hoge/MycoTAPLocal
+THREAD=10
+LOGFILE="$2/mycotap.log"
+if [ $# -ne 2 ]; then
+  echo "you specified $# arguments." 1>&2
+  echo "But this program can only use 2 arguments." 1>&2
+  exit 1
+fi
+if [ ! -f $1 ]; then
+	echo "No $1 file exist." 1>&2
+	exit 1
+fi
+if [ ! -d $2 ]; then
+	echo "$2 directory does not exist. please mkdir" 1>&2
+	exit 1
+fi
+
+touch $LOGFILE
+ if [ ! -f $LOGFILE ]; then
+  echo "Error cannot create $LOGFILE."
+  exit 1;
+fi
+
+{
+DE0=`basename "$1"`
+cp "$1" "$2/$DE0"
+$PROGRAMDIR/fastp -i $2/$DE0 -o $2/$DE0.rem.fastq -G -3 -n 1 -l 90 -w 1 -x
+$PROGRAMDIR/seqkit fq2fa $2/$DE0.rem.fastq > $2/$DE0.rem.fastq.fasta
+$PROGRAMDIR/vsearch --usearch_global $2/$DE0.rem.fastq.fasta --db $UNITEREF1 --id 0.97 --query_cov 0.8 --strand both --blast6out $2/$DE0.rem.fastq.fasta.UNITE.97withspecies -thread $THREAD
+perl $PROGRAMDIR/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97withspecies
+$PROGRAMDIR/vsearch --usearch_global $2/$DE0.rem.fastq.fasta --db $UNITEREF2 --id 0.97 --query_cov 0.8 --strand both --blast6out $2/$DE0.rem.fastq.fasta.UNITE.97nospecies -thread $THREAD
+perl $PROGRAMDIR/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies
+perl $PROGRAMDIR/RemoveNameDup.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies $2/$DE0.rem.fastq.fasta.UNITE.97withspecies
+perl $PROGRAMDIR/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies.withoutdup
+rm -f $2/$DE0.rem.fastq
+rm -f $2/$DE0.rem.fastq.fasta
+} >> "$LOGFILE" 2>&1
