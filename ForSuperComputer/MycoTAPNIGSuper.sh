8e1c56d85930c32ed9a9681022d58f075d5dd7ec
Tazawa(Axio) <49433886+satoshi0409@users.noreply.github.com>
2021-07-08 13:11:58 +0900

    Update MycoTAPNIGSuper.sh
    change the path to UNITEREF1/2 and set the path to sif file to execute perl scripts inside the sif file.

--- ForSuperComputer/MycoTAPNIGSuper.sh
+++ ForSuperComputer/MycoTAPNIGSuper.sh
@@ -4,9 +4,9 @@
 # USAGE: this_shell_script input_file output_dir
 # input_file=$1
 # output_dir=$2
-UNITEREF1=$PWD/UNITERefS.20200204.fasta.withspecies.fasta.NoN.fasta.gz
-UNITEREF2=$PWD/UNITERefS.20200204.fasta.sp.fasta.NoN.fasta.gz
-PROGRAMDIR=$PWD
+UNITEREF1=/lustre6/public/app/myco-tap/UNITERefS.20200204.fasta.withspecies.fasta.NoN.fasta.gz
+UNITEREF2=/lustre6/public/app/myco-tap/UNITERefS.20200204.fasta.sp.fasta.NoN.fasta.gz
+SIF_PATH=/lustre6/public/app/myco-tap/MycoTAP-dev20210707.sif
 
 THREAD=1
 LOGFILE="$2/mycotap.log"
@@ -16,12 +16,12 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 if [ ! -f $1 ]; then
-	echo "No $1 file exist." 1>&2
-	exit 1
+        echo "No $1 file exist." 1>&2
+        exit 1
 fi
 if [ ! -d $2 ]; then
-	echo "$2 directory does not exist. please mkdir" 1>&2
-	exit 1
+        echo "$2 directory does not exist. please mkdir" 1>&2
+        exit 1
 fi
 
 touch $LOGFILE
@@ -35,12 +35,13 @@ DE0=`basename "$1"`
 cp "$1" "$2/$DE0"
 singularity exec /usr/local/biotools/f/fastp\:0.20.1--h8b12597_0 fastp -i $2/$DE0 -o $2/$DE0.rem.fastq -G -3 -n 1 -l 90 -w 1 -x
 singularity exec /usr/local/biotools/s/seqkit\:0.13.2--0 seqkit fq2fa $2/$DE0.rem.fastq > $2/$DE0.rem.fastq.fasta
-singularity exec /usr/local/biotools/v/vsearch\:2.15.2--h2d02072_0 vsearch --usearch_global $2/$DE0.rem.fastq.fasta --db $UNITEREF1 --id 0.97 --query_cov 0.8 --strand both --blast6out $2/$DE0.rem.fastq.fasta.UNITE.97withspecies --threads $THREAD
-perl $PROGRAMDIR/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97withspecies
-singularity exec /usr/local/biotools/v/vsearch\:2.15.2--h2d02072_0 vsearch --usearch_global $2/$DE0.rem.fastq.fasta --db $UNITEREF2 --id 0.97 --query_cov 0.8 --strand both --blast6out $2/$DE0.rem.fastq.fasta.UNITE.97nospecies --threads $THREAD
-perl $PROGRAMDIR/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies
-perl $PROGRAMDIR/RemoveNameDup.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies $2/$DE0.rem.fastq.fasta.UNITE.97withspecies
-perl $PROGRAMDIR/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies.withoutdup
+singularity exec -B /lustre6/public/app/myco-tap /usr/local/biotools/v/vsearch\:2.15.2--h2d02072_0 vsearch --usearch_global $2/$DE0.rem.fastq.fasta --db $UNITEREF1 --id 0.97 --query_cov 0.8 --strand both --blast6out $2/$DE0.rem.fastq.fasta.UNITE.97withspecies --threads $THREAD
+
+singularity exec $SIF_PATH perl /usr/local/bin/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97withspecies
+singularity exec -B /lustre6/public/app/myco-tap  /usr/local/biotools/v/vsearch\:2.15.2--h2d02072_0 vsearch --usearch_global $2/$DE0.rem.fastq.fasta --db $UNITEREF2 --id 0.97 --query_cov 0.8 --strand both --blast6out $2/$DE0.rem.fastq.fasta.UNITE.97nospecies --threads $THREAD
+singularity exec $SIF_PATH perl /usr/local/bin/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies
+singularity exec $SIF_PATH perl /usr/local/bin/RemoveNameDup.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies $2/$DE0.rem.fastq.fasta.UNITE.97withspecies
+singularity exec $SIF_PATH perl /usr/local/bin/NameSum.pl $2/$DE0.rem.fastq.fasta.UNITE.97nospecies.withoutdup
 rm -f $2/$DE0.rem.fastq
 rm -f $2/$DE0.rem.fastq.fasta
 } >> "$LOGFILE" 2>&1
